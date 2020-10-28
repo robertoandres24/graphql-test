e518abf4ef0787e263e7f7be1ccc2ebe0d95c8a8
robertoandres24 <robertoandres24@gmail.com>
2020-10-28 15:14:02 -0300

    query with params

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 var { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -8,9 +8,11 @@ const app = express()
 const port = process.env.port || 4000
 
 // define schema
-const schema = buildSchema(
-  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
-)
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
 
 // execute query
 // graphql(schema, '{ welcome }', resolvers).then((data) => console.log(data))
