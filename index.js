6e61023a266cb6e6226b304e317c65b46d9fa77e
robertoandres24 <robertoandres24@gmail.com>
2020-10-28 00:04:07 -0300

    add api express

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { graphql, buildSchema } = require('graphql')
+var { graphqlHTTP } = require('express-graphql')
+const express = require('express')
+const app = express()
+const port = process.env.port || 4000
 
 // define schema
-
 const schema = buildSchema(`
 	type Query {
 		hello: String,
@@ -12,8 +15,21 @@ const schema = buildSchema(`
 // define resolvers
 const resolvers = {
   hello: () => 'Hello world',
-  welcome: () => 'Welcome to my world',
+  welcome: () => 'Welcome to my changed world',
 }
 
 // execute query
-graphql(schema, '{ welcome }', resolvers).then((data) => console.log(data))
+// graphql(schema, '{ welcome }', resolvers).then((data) => console.log(data))
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+)
+
+app.listen(port, () =>
+  console.log(`Now browse to http://localhost:${port}/graphql`)
+)
