d56acb361b05e2c78ca4097511d96afa641fa41d
robertoandres24 <robertoandres24@gmail.com>
2020-10-29 15:37:32 -0300

    add nested students in course, and get populate in query

--- database/models/Course.js
+++ database/models/Course.js
@@ -4,5 +4,6 @@ const { Schema } = mongoose
 const courseSchema = new Schema({
   title: String,
   length: String,
+  people: [{ type: Schema.Types.ObjectId, ref: 'Student' }],
 })
 module.exports = mongoose.model('Course', courseSchema)
--- database/models/Student.js
+++ database/models/Student.js
@@ -4,5 +4,6 @@ const { Schema } = mongoose
 const studentSchema = new Schema({
   name: String,
   email: String,
+  course: { type: Schema.Types.ObjectId, ref: 'Course' },
 })
 module.exports = mongoose.model('Student', studentSchema)
